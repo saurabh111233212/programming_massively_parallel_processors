9195b2535ce57c70e19ccc1bb68c3da928fd1d72
Saurabh Shah <saurabh2@apple.com>
2024-05-26 12:05:08 -0400

    more cuda setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
-sudo apt-get update
-sudo apt-get install -y cuda
+apt-get update
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/cuda-repo-ubuntu2004-11-2-local_11.2.2-460.32.03-1_amd64.deb
+dpkg -i cuda-repo-ubuntu2004-11-2-local_11.2.2-460.32.03-1_amd64.deb
+apt-key add /var/cuda-repo-ubuntu2004-11-2-local/7fa2af80.pub
+apt-get install -y cuda
 export PATH=/usr/local/cuda/bin:$PATH
 source ~/.bashrc
-source ~/.zshrc
 nvcc --version
